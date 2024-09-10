dfeb7d7a9af63b959bde7da8ebc028af1ba4bd1f
Skyy Moore <124527392+mooreApps22@users.noreply.github.com>
2024-09-10 16:36:49 +0100

    Update data.h

--- data.h
+++ data.h
@@ -4,12 +4,12 @@
 
 // A process is a single process
 typedef	struct s_process 
-{
-	struct process	*next;		// next process in pipeline
-	char			**cmdv;		// for the execve() 2nd arg
+{	
+	struct process	*next;				// next process in pipeline
+	char			**cmdv;			// for the execve() 2nd arg
 	pid_t			pid;
-	char			completed;	// true if process has completed
-	char			stopped;	// true if process has stopped 
+	char			completed;		// true if process has completed
+	char			stopped;		// true if process has stopped 
 	int				status;		// exit status
 } t_process;
 
@@ -19,13 +19,13 @@ typedef	struct s_job
 {
 	struct job		*next;			// next active job
 	char			cmd;			// command line, used for messages
-	t_process		*head_process;	// list of process in job
+	t_process		*head_process;		// list of process in job
 	pid_t			pgid;			// process group ID
 	char			notified;		// true if user told about stopped job
-	struct termios	tmodes;			// saved terminal modes
-	int				stdin;
-	int				stdout;
-	int				stderr;
+	struct termios		tmodes;			// saved terminal modes
+	int			stdin;
+	int			stdout;
+	int			stderr;
 } t_job;
 
 #endif
